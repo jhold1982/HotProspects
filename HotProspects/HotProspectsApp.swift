d7426fe0e977379237a2886cb9d843e7ed395bb9
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-11-08 11:17:26 -0600

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Justin Hold on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
